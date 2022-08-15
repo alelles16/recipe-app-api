dc4253a7bfa42cc2284878353b706f6109c23efa
Nani <alelles16@hotmai.com>
2022-08-14 23:56:43 -0500

    Add DB service into docker-compose

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """
+    Sum between two numbers
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    Subtract between two numbers
+    """
+    return abs(x - y)
